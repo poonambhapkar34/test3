61d97d6643ab2d3f52c85dc988f37ebf24bae53e
poonambhapkar34 <poonambhapkar34@gmail.com>
2022-12-24 17:41:45 +0530

    added html css files

---
+++ javascript/first.js
@@ -0,0 +1,28 @@
+//let var const
+//syntax : let/var/const variable_name = data;
+let b = 10; //declaration + defination
+let a;  //declaration
+a = 10; //defination
+let c=50,d=30,t=80;
+
+console.log('c',c);
+console.log(d,'d');
+let name1 = "poonam";
+console.log(name1);
+let my_name = "poonam1";
+let myName = "poonam3";
+let MYNAME = "poonam4";
+console.log(a);
+console.log('a',a);
+console.log(a,'a');
+console.log('a' + a);
+console.log("a" + a);
+console.log('a'+a+b);
+console.log('a'+a-MYNAME);
+console.log('a',a+b);
+console.log('a',a+MYNAME);
+console.log('a-->',a + 'b-->',b);
+let u='30';
+console.log(u);
+console.log(u*20);
+console.log('xyx'+20);
